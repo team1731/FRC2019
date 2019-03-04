b3d845fd5266e3a3db4f08a1a94929c318c9a067
daniel <rdanielbuckley@gmail.com>
2019-03-03 22:18:53 -0500

    Wade and Stephen's Vision Code

--- VEAL_FARMSTRONG/src/main/java/org/usfirst/frc/team1731/robot/loops/RobotStateEstimator.java
+++ VEAL_FARMSTRONG/src/main/java/org/usfirst/frc/team1731/robot/loops/RobotStateEstimator.java
@@ -2,9 +2,14 @@ package org.usfirst.frc.team1731.robot.loops;
 
 import org.usfirst.frc.team1731.lib.util.math.Rotation2d;
 import org.usfirst.frc.team1731.lib.util.math.Twist2d;
+import org.usfirst.frc.team1731.robot.ControlBoardInterface;
+import org.usfirst.frc.team1731.robot.GamepadControlBoard;
 import org.usfirst.frc.team1731.robot.Kinematics;
 import org.usfirst.frc.team1731.robot.RobotState;
 import org.usfirst.frc.team1731.robot.subsystems.Drive;
+
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Periodically estimates the state of the robot using the robot's distance traveled (compares two waypoints), gyroscope
@@ -25,14 +30,38 @@ public class RobotStateEstimator implements Loop {
     double left_encoder_prev_distance_ = 0;
     double right_encoder_prev_distance_ = 0;
 
+    //#region Vision Camera Variables
+    boolean visionCamAvailable = false;
+    double visionCamXPosition;
+
+    public boolean GetVisionCamAvailable(){
+        return visionCamAvailable;
+    }
+
+    public double GetVisionCamXPosition(){
+        return visionCamXPosition;
+    }
+    //#endregion
+
+
     @Override
     public synchronized void onStart(double timestamp) {
         left_encoder_prev_distance_ = drive_.getLeftDistanceInches();
         right_encoder_prev_distance_ = drive_.getRightDistanceInches();
+        AttemptVisionCamConnection();
+    }
+
+    private ControlBoardInterface mControlBoard = GamepadControlBoard.getInstance();
+    private SerialPort visionCam;
+
+    private void AttemptVisionCamConnection(){
+        visionCam = new SerialPort(115200, SerialPort.Port.kUSB2);
     }
 
     @Override
     public synchronized void onLoop(double timestamp) {
+        //#region Original Estimator Code
+
         final double left_distance = drive_.getLeftDistanceInches();
         final double right_distance = drive_.getRightDistanceInches();
         final Rotation2d gyro_angle = drive_.getGyroAngle();
@@ -43,6 +72,24 @@ public class RobotStateEstimator implements Loop {
         robot_state_.addObservations(timestamp, odometry_velocity, predicted_velocity);
         left_encoder_prev_distance_ = left_distance;
         right_encoder_prev_distance_ = right_distance;
+
+        //#endregion
+
+        //#region Vision Camera
+
+        boolean tracktorDrive = mControlBoard.getTractorDrive();
+
+        if(visionCam == null){
+            AttemptVisionCamConnection();
+        } else {
+            String visionTargetPositions_Raw = visionCam.readString();
+            String[] visionTargetPositions = visionTargetPositions_Raw.split(",");
+            visionCamAvailable = visionTargetPositions[0].length() > 0 && visionTargetPositions[0] != null;
+            SmartDashboard.putString("RawVisionCamData_Raw", visionTargetPositions_Raw);
+            visionCamXPosition = Double.valueOf(visionTargetPositions[0]);
+        }
+
+        //#endregion
     }
 
     @Override
